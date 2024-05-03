98bb07fc96a0a5e8515f52750e8d9cb017b6156e
John Keen <blueisgoodtoo@gmail.com>
2024-05-03 13:24:42 -0700

    Update db_connect to use Heroku config vars for mysqli.

--- db_connect.php
+++ db_connect.php
@@ -4,11 +4,11 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 // modify these settings according to the account on your database server.
-$host = "localhost";
-$port = "3306";
-$username = "root";
-$user_pass = "root";
-$database_in_use = "jokesdb";
+$host = getenv('DB_HOST');
+$port = getenv('DB_PORT');
+$username = getenv('DB_USER');
+$user_pass = getenv('DB_PASSWORD');
+$database_in_use = getenv('DB_DATABASE_NAME');
 
 
 $mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
